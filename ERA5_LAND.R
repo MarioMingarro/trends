be6aa1b6bd87a673d5196dd4f3cd4511edab44b3
Mario Mingarro <60926549+MarioMingarro@users.noreply.github.com>
2024-05-27 22:42:19 +0200

    new

--- ERA5_LAND.R
+++ ERA5_LAND.R
@@ -11,51 +11,34 @@ library(foreach)
 library(VoCC)
 library(tidyterra)
 
+rm(list=(ls()[ls()!="data"]))
+
 # DATA ----
-a <- rast("B:/A_DATA/ERA_5/ERA_5_1940_1987.nc")
-b <- rast("B:/A_DATA/ERA_5/ERA_5_1988_2023.nc")
-b <- b[[c(1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,47,49,51,53,55,57,59,61,63,65,67,69,71,73,75,
-          77,79,81,83,85,87,89,91,93,95,97,99,101,103,105,107,109,111,113,115,117,119,121,123,125,127,129,131,133,135,
-          137,139,141,143,145,147,149,151,153,155,157,159,161,163,165,167,169,171,173,175,177,179,181,183,185,187,189,
-          191,193,195,197,199,201,203,205,207,209,211,213,215,217,219,221,223,225,227,229,231,233,235,237,239,241,243,
-          245,247,249,251,253,255,257,259,261,263,265,267,269,271,273,275,277,279,281,283,285,287,289,291,293,295,297,
-          299,301,303,305,307,309,311,313,315,317,319,321,323,325,327,329,331,333,335,337,339,341,343,345,347,349,351,
-          353,355,357,359,361,363,365,367,369,371,373,375,377,379,381,383,385,387,389,391,393,395,397,399,401,403,405,
-          407,409,411,413,415,417,419,421,423,425,427,429,431,433,435,437,439,441,443,445,447,449,451,453,455,457,459,
-          461,463,465,467,469,471,473,475,477,479,481,483,485,487,489,491,493,495,497,499,501,503,505,507,509,511,513,
-          515,517,519,521,523,525,527,529,531,533,535,537,539,541,543,545,547,549,551,553,555,557,559,561,563,565,567,
-          569,571,573,575,577,579,581,583,585,587,589,591,593,595,597,599,601,603,605,607,609,611,613,615,617,619,621,
-          623,625,627,629,631,633,635,637,639,641,643,645,647,649,651,653,655,657,659,661,663,665,667,669,671,673,675,
-          677,679,681,683,685,687,689,691,693,695,697,699,701,703,705,707,709,711,713,715,717,719,721,723,725,727,729,
-          731,733,735,737,739,741,743,745,747,749,751,753,755,757,759,761,763,765,767,769,771,773,775,777,779,781,783,
-          785,787,789,791,793,795,797,799,801,803,805,807,809,811,813,815,817,819,821,823,825,827,829,831,833,835,837,
-          839)]]
+a <- rast("T:/ERA5_DATA/era5_1940_2023.nc")
 
-a <- tapp(a, "years", max)
-a <- a-273.15
-b <- tapp(b, "years", max)
+n_layers <- nlyr(a)
+
+n_layers <- seq(1, n_layers, by = 2)
+
+a <- a[[n_layers]]
+rm(n_layers)
+
+tic()
+b <- terra::tapp(a, "years", max)
 b <- b-273.15
-
-data <- c(a,b)
-data <- rotate(data)
-
-crs(data)  <- "epsg:4326"
+toc() # 15 sec
 
 
-land <- vect("A:/ERA5_TEST/ne_10m_land.shp")
-ocean <- vect("A:/ERA5_TEST/ne_10m_ocean.shp")
 
-
-terra::ext(data) <- terra::ext(ocean)
+data <- rotate(b)
 
 crs(data)  <- "epsg:4326"
 
-# Land 
-data <- terra::mask(data, land)
 
 data <- as.data.frame(data, xy = TRUE)
-
-
+data <- data[, -87]
+data2 <- data
+data <- data[490481,]
 # BP ANALYSIS ----
 tic()
 n.cores <- parallel::detectCores() - 1
@@ -71,10 +54,10 @@ res <- foreach(i = c(1:nrow(data)), # 1:nrow(data) 1:2500  # 2500:5000 # 5000:75
   ss <- as.vector(data[i,-c(1,2)])
   ss <- ts(t(ss),
            start = 1940,
-           end = 2022,
+           end = 2023,
            frequency = 1)
   #year
-  qlr <- strucchange::Fstats(ss ~ 1, data = ss) #Quandt Likelihood Ratio (QLR)
+  qlr <- strucchange::Fstats(ss ~ 1, data = ss, from = 0.05) #Quandt Likelihood Ratio (QLR)
   bp <- strucchange::breakpoints(qlr)
   year_break <- strucchange::breakdates(bp)
   RSS <- bp$RSS
@@ -83,23 +66,26 @@ res <- foreach(i = c(1:nrow(data)), # 1:nrow(data) 1:2500  # 2500:5000 # 5000:75
   year_pre <- seq(1940, year_break, 1)
   lm_pre <- lm(pre ~ year_pre)
   P_pre <- round(lm_pre$coefficients[2],4)
+  Pv_pre <- round(summary(lm_pre)$coefficients[2, 4],4)
   AIC_pre <- AIC(lm_pre)
   BIC_pre <- BIC(lm_pre)
   RSE_pre <- sqrt(deviance(lm_pre)/df.residual(lm_pre))
   
   ##post
   post <- ss[bp$breakpoints:length(ss)]
-  year_post <- seq(year_break,2022,1)
+  year_post <- seq(year_break,2023,1)
   lm_post <- lm(post ~ year_post)
   P_post <- round(lm_post$coefficients[2],4)
+  Pv_post <- round(summary(lm_post)$coefficients[2, 4],4)
   AIC_post <- AIC(lm_post)
   BIC_post <- BIC(lm_post)
   RSE_post <- sqrt(deviance(lm_post)/df.residual(lm_post))
   
   ##general
-  year_total <- seq(1940,2022,1)
+  year_total <- seq(1940,2023,1)
   lm_total <- lm(ss ~ year_total)
   P_total <- round(lm_total$coefficients[2],4)
+  Pv_total <- round(summary(lm_post)$coefficients[2, 4],4)
   AIC_total <- AIC(lm_total)
   BIC_total <- BIC(lm_total)
   RSE_total <- sqrt(deviance(lm_total)/df.residual(lm_total))
@@ -110,19 +96,25 @@ res <- foreach(i = c(1:nrow(data)), # 1:nrow(data) 1:2500  # 2500:5000 # 5000:75
   p.value <- test[2]
   #sa.cusum <- strucchange::efp(ss ~ 1, data = ss, type = "OLS-CUSUM")
   data.frame(year_break, RSS, F.sup, p.value, 
-             P_total, AIC_total, BIC_total, RSE_total,
-             P_pre, AIC_pre, BIC_pre,RSE_pre,
-             P_post,AIC_post, BIC_post, RSE_post)
+             P_total, Pv_total, AIC_total, BIC_total, RSE_total,
+             P_pre, Pv_pre, AIC_pre, BIC_pre,RSE_pre,
+             P_post, Pv_post,AIC_post, BIC_post, RSE_post)
 }
 
 parallel::stopCluster(cl = my.cluster)
 toc()
-resultados_land <- cbind(data, res)
+
+res <- cbind(data, res)
+resultados <- res
 
 
+a <- names(resultados)
+b <- unique(names(resultados))
+write.csv2(resultados_land,"D:/ERA_5_2024/resultados_ERA5_max2.csv" )
 
-write.csv2(resultados_land,"A:/ERA5_RESULTS/resultados_land_max.csv" )
 
+sig <- dplyr::filter(resultados, p.value <= 0.01)
+sig <- dplyr::filter(resultados, resultados$ <= 0.01)
 
 # PLOTTING ----
 
@@ -149,7 +141,7 @@ write.csv2(resultados_land,"A:/ERA5_RESULTS/resultados_land_max.csv" )
 s <- c("year_break", "statistic", "p.value", "P_total", "RSE_total", "P_pre", "RSE_pre", "P_post", "RSE_post")
 f <- c("x", "y", "year_break", "statistic", "p.value", "P_total", "RSE_total", "P_pre", "RSE_pre", "P_post", "RSE_post")
 
-resultados_land <- subset(resultados_land, select=f)
+resultados_land <- subset(sig, select=f)
 
 
 for (i in 1:length(s)){
@@ -158,20 +150,20 @@ for (i in 1:length(s)){
   r <- rast(r, type="xyz")
   crs(r)  <- "epsg:4326"
   
- writeRaster(r,paste0("A:/ERA5_RESULTS/RASTER/LAND/", s[i] ,"_max.tif" ))
+ writeRaster(r,paste0("D:/ERA_5_2024/MAPS/SIG/", s[i] ,"_max.tif" ))
 
 r <- project(r,"+proj=hatano", mask = TRUE)
 
 
 # Discretize for better plotting after projection
-g <- st_graticule(ndiscr = 1000) |> st_transform(st_crs(r))
+g <- st_graticule(ndiscr = 1000) %>%  st_transform(st_crs(r))
 
-border <- st_graticule() |>
-  st_bbox() |>
-  st_as_sfc() |>
-  st_transform(3857) |>
-  st_segmentize(500000) |>
-  st_transform(st_crs(r)) |>
+border <- st_graticule() %>% 
+  st_bbox()%>%
+  st_as_sfc()%>%
+  st_transform(3857)%>%
+  st_segmentize(500000)%>%
+  st_transform(st_crs(r))%>%
   st_cast("POLYGON")
 
 # Labels
@@ -201,6 +193,6 @@ print(ggplot() +
   theme_void() +
   labs(x = "", y = "", fill = paste0(s[i])))
 
-ggsave(paste0("A:/ERA5_RESULTS/PLOT/LAND/", s[i] ,"_max.jpeg" ), dpi = 600, width = 30, height = 30, units = "cm")
+ggsave(paste0("D:/ERA_5_2024/MAPS/SIG/", s[i] ,"_max.jpeg" ), dpi = 600, width = 30, height = 30, units = "cm")
 
 }
