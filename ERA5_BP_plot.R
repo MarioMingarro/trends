94800ce2eb0ed5469cdf4a46dd79e7aa8c05da11
Mario Mingarro <mario_mingarro@hotmail.com>
2024-07-10 12:25:20 +0200

    new

--- ERA5_BP_plot.R
+++ ERA5_BP_plot.R
@@ -25,15 +25,15 @@ final <- final[,-1]
 
 
 # Seleccionar caso especifico
-fila <- 19698
+fila <- 622977
 ss <-  data[fila,]
 tt <- final[fila,]
 
 #5 385341
-#4 365220
-#3 380883
-#2 32027 189749
-#1 13249  19698
+#4 365220 829429 586578 566396
+#3 380883 750951 622977
+#2 32027 189749 225096
+#1 13249  19698 198000
 #0 396964
 ###### -----
 ss <- as.numeric(ss[, -c(1, 2)])
@@ -100,6 +100,7 @@ map <- ggplot() +
 
 
 #Tabla
+tt <- final[fila,]
 tt <- tt %>%
   pivot_longer(
     cols = matches("year_break_\\d+|P_pre_\\d+|Pv_pre_\\d+|RSE_pre_\\d+|P_post_\\d+|Pv_post_\\d+|RSE_post_\\d+"),
@@ -111,7 +112,7 @@ tt <- tt %>%
 tt <- tt %>%
   arrange(x, y, year_break)
 tt <- tt[, c(9,11)]
-
+tt <- tt[-c(9,10,11,16,17,18,23,24,25,30,31,32),]
 
 # Identificar los bloques por year_break
 tt$group <- cumsum(tt$metric == "year_break")
@@ -131,13 +132,37 @@ tk <- ttheme_minimal(
   colhead=list(fg_params=list(col="gray30", fontface=4L)),
   rowhead=list(fg_params=list(col="gray50", fontface=3L)))
 table <- tableGrob(tt, theme = tk) 
+# BIC
+
+bic <- summary(bp)
+bic <- as.data.frame(t(bic$RSS))
+bic <- dplyr::mutate(bic, "breaks" = rownames(bic))
 
 
+# bic <- ggplot(bic, aes(x = breaks)) +
+#   geom_line(aes(y = BIC, color = "BIC")) +
+#   geom_point(aes(y = BIC, color = "BIC")) +
+#   geom_line(aes(y = RSS * 100, color = "RSS")) +  # Escalamos RSS para mejor visualización
+#   geom_point(aes(y = RSS * 100, color = "RSS")) +  # Escalamos RSS para mejor visualización
+#   scale_y_continuous(sec.axis = sec_axis(~./100, name = "RSS")) +  # Agregar segundo eje y para RSS
+#   labs(
+#     x = "Número de Puntos de Ruptura",
+#     y = "BIC",
+#     color = "Métrica"
+#   ) +
+#   scale_color_manual(values = c("BIC" = "blue", "RSS" = "red")) + 
+#   theme_minimal()  # Estilo del gráfico
+
+
+table_bic <- tableGrob(round(bic[,c(2,1)],4), theme = tk) 
 
 ###### -----
 # Todos
-ggarrange(trend_plot, map,
+ggarrange(trend_plot, table_bic, map,
           table)
+
+
+
 
 # Histogram -----
 final <- read.csv2("C:/A_TRABAJO/ERA5/RESULT_ERA5_1940_2023.csv")
@@ -209,3 +234,29 @@ test2 <- as.data.frame(t(rbind(test2, year)))
 k <- lm(V1~V2, data = test2)
 summary(k)
 writexl::write_xlsx(test, "C:/A_TRABAJO/ERA5/kkdvk2.xlsx")
+
+plot(bp)
+
+
+## plot segements 
+
+# Obtener los puntos de quiebre
+breakpoints <- bp$breakpoints
+
+# Crear un dataframe a partir de la serie temporal
+ts_data <- data.frame(
+  time = time(ss),
+  value = as.numeric(ss)
+)
+
+# Agregar un indicador de segmento a los datos
+ts_data$segment <- cut(1:nrow(ts_data), breaks = c(0, breakpoints, nrow(ts_data)), labels = FALSE, include.lowest = TRUE)
+
+ggplot(ts_data, aes(x = time, y = value)) +
+  geom_line(aes(color = factor(segment))) +
+  geom_vline(xintercept = ts_data$time[breakpoints], linetype = "dashed") +
+  labs(title = "Segmentos de la Serie Temporal con Puntos de Quiebre",
+       x = "Tiempo",
+       y = "Valor",
+       color = "Segmento") +
+  theme_minimal()
