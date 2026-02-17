3e0fe6da3e0e704204bcd2bfe247f27f45a04efc
Mario Mingarro <mario_mingarro@hotmail.com>
2026-02-17 20:46:25 +0100

    n

--- ERA5_BP_1940_2025.R
+++ ERA5_BP_1940_2025.R
@@ -5,129 +5,84 @@ library(doParallel)
 library(foreach)
 library(tictoc)
 
-# --- 1. Definir la función ANTES de usarla ---
-bp_analysis <- function(i, data, start_year, end_year) {
-  # Obtener los datos de la serie temporal (quitando columnas lon/lat)
-  ss_vals <- as.numeric(data[i, -c(1, 2)])
-  
-  # Manejo de NAs y longitud de serie
-  if(any(is.na(ss_vals))) {
-    # Retornar una fila vacía con la misma estructura si hay NAs
-    return(data.frame(matrix(NA, nrow=1, ncol=48))) # Ajustar ncol a los resultados
-  }
-  
+
+bp_analysis_with_test <- function(i, data_matrix, start_year, end_year) {                
+  ss_vals <- data_matrix[i, ]
   ss <- ts(ss_vals, start = start_year, end = end_year, frequency = 1)
+  years_seq <- seq(start_year, end_year, 1)
   
-  # Detección de rupturas
-  bp <- strucchange::breakpoints(ss ~ 1)
+  # 1. Detección de rupturas (necesario para breakdates)
+  bp <- strucchange::breakpoints(ss ~ 1) 
   breakdates <- strucchange::breakdates(bp)
+  n_breaks <- length(breakdates)
   
-  # Inicializar resultados
+  # 2. prueba de estabilidad
+  qlr <- strucchange::Fstats(ss ~ 1, from = 0.03) #Quandt Likelihood Ratio (QLR)
+  test <- strucchange::sctest(qlr, type = "supF")
+  
+  # 3. Análisis total
+  lm_total <- lm(ss ~ years_seq)
+  
   results <- list(
-    year_break_1 = 999, year_break_2 = 999, year_break_3 = 999, year_break_4 = 999, year_break_5 = 999,                
-    n_breaks = length(breakdates), F_sup = NA, p_value = NA,
-    P_total = NA, Pv_total = NA, RSE_total = NA, tmed_total = NA
+    n_breaks = n_breaks,
+    F_sup = round(test$statistic, 4),
+    p_value = round(test$p.value, 4),
+    P_total = round(coef(lm_total)[2], 4),
+    Pv_total = round(summary(lm_total)$coefficients[2, 4], 4),
+    RSE_total = round(sqrt(deviance(lm_total) / df.residual(lm_total)), 4),
+    tmed_total = round(mean(ss), 2)
   )
   
-  # Inicializar segmentos pre/post hasta 6 segmentos posibles (5 rupturas)
-  for(k in 1:6) {
-    results[[paste0("P_pre_", k)]] <- NA
-    results[[paste0("Pv_pre_", k)]] <- NA
-    results[[paste0("RSE_pre_", k)]] <- NA
-    results[[paste0("tmed_pre_", k)]] <- NA
-    results[[paste0("P_post_", k)]] <- NA
-    results[[paste0("Pv_post_", k)]] <- NA
-    results[[paste0("RSE_post_", k)]] <- NA
-    results[[paste0("tmed_post_", k)]] <- NA
-  }
-  
-  # Análisis estadístico de rupturas
-  qlr <- strucchange::Fstats(ss ~ 1, from = 0.05) # 'from' ajustado para series cortas
-  test <- strucchange::sctest(qlr, type = "supF")
-  results$F_sup <- test$statistic
-  results$p_value <- test$p.value
-  
-  # Análisis total
-  years_seq <- seq(start_year, end_year, 1)
-  lm_total <- lm(ss ~ years_seq)
-  results$P_total <- round(coef(lm_total)[2], 4)
-  results$Pv_total <- round(summary(lm_total)$coefficients[2, 4], 4)
-  results$RSE_total <- round(sqrt(deviance(lm_total) / df.residual(lm_total)), 4)
-  results$tmed_total <- round(mean(ss), 2)
-  
-  # Análisis por segmentos
-  if (length(breakdates) > 0) {
-    for (j in 1:length(breakdates)) {
-      results[[paste0("year_break_", j)]] <- breakdates[j]
+  # 4. Análisis por segmentos (igual que antes)
+  if (n_breaks > 0) {
+    pts <- c(1, bp$breakpoints, length(ss))
+    
+    for (j in 1:(length(pts) - 1)) {
+      idx_start <- pts[j]
+      idx_end <- pts[j+1]
+      if (j > 1) idx_start <- idx_start + 1
       
-      # Definir índices de segmento
-      idx_start <- if (j == 1) 1 else bp$breakpoints[j-1] + 1
-      idx_end <- bp$breakpoints[j]
+      segment <- ss[idx_start:idx_end]
+      years_seg <- years_seq[idx_start:idx_end]
       
-      # Segmento Pre
-      segment_pre <- ss[idx_start:idx_end]
-      years_pre <- years_seq[idx_start:idx_end]
-      if(length(segment_pre) > 2) { # lm necesita > 2 puntos
-        lm_pre <- lm(segment_pre ~ years_pre)
-        results[[paste0("P_pre_", j)]] <- round(coef(lm_pre)[2], 4)
-        results[[paste0("Pv_pre_", j)]] <- round(summary(lm_pre)$coefficients[2, 4], 4)
-        results[[paste0("RSE_pre_", j)]] <- round(sqrt(deviance(lm_pre) / df.residual(lm_pre)), 4)
-        results[[paste0("tmed_pre_", j)]] <- round(mean(segment_pre), 2)
-      }
-      
-      # Segmento Post
-      idx_post_start <- bp$breakpoints[j]
-      idx_post_end <- if (j == length(breakdates)) length(ss) else bp$breakpoints[j+1]
-      
-      segment_post <- ss[idx_post_start:idx_post_end]
-      years_post <- years_seq[idx_post_start:idx_post_end]
-      
-      if(length(segment_post) > 2) {
-        lm_post <- lm(segment_post ~ years_post)
-        results[[paste0("P_post_", j)]] <- round(coef(lm_post)[2], 4)
-        results[[paste0("Pv_post_", j)]] <- round(summary(lm_post)$coefficients[2, 4], 4)
-        results[[paste0("RSE_post_", j)]] <- round(sqrt(deviance(lm_post) / df.residual(lm_post)), 4)
-        results[[paste0("tmed_post_", j)]] <- round(mean(segment_post), 2)
+      if(length(segment) > 2) {
+        lm_seg <- lm(segment ~ years_seg)
+        results[[paste0("P_seg_", j)]] <- round(coef(lm_seg)[2], 4)
+        results[[paste0("Pv_seg_", j)]] <- round(summary(lm_seg)$coefficients[2, 4], 4)
+        results[[paste0("year_start_", j)]] <- years_seq[idx_start]
+        results[[paste0("year_end_", j)]] <- years_seq[idx_end]
       }
     }
   }
   
   return(as.data.frame(results))
 }
-
-# --- 2. Cargar y Preprocesar (Corregir años) ---
 a <- rast("C:/A_TRABAJO/A_JORGE/ERA5/ERA5_DATA/era5_1940_2025.nc")
+ ext_test <- ext(-10, 0, 35, 45)
+ a <- crop(a, ext_test)
 
-
-# 1. Crear la secuencia de fechas correcta
-# Asegúrate de que el número de fechas sea igual a nlyr(a)
 n_capas <- nlyr(a)
 fechas <- seq(as.Date("1940-01-01"), by = "month", length.out = n_capas)
 
-# 2. Asignar el tiempo usando terra::time
 terra::time(a) <- fechas
 
-# 3. Ahora sí funcionará tapp
-b <- terra::tapp(a, "years", max)
+
+b <- terra::tapp(a, "years", mean)
 b <- b - 273.15
 data_rast <- terra::rotate(b)
 crs(data_rast) <- "epsg:4326"
 
 
-
 df_data <- as.data.frame(data_rast, xy = TRUE)
-# Limpieza de datos (depende de tu dataset)
 df_data <- na.omit(df_data) 
 
-# --- 3. Ejecución Paralela ---
-numCores <- detectCores() - 1
+numCores <- 10
 cl <- makeCluster(numCores)
 registerDoParallel(cl)
 
 tic()
 res <- foreach(p = 1:nrow(df_data), .combine = rbind, .packages = c("strucchange", "stats")) %dopar% {
-  # Definir años reales de tus datos aquí
-  bp_analysis(p, df_data, start_year = 1940, end_year = 2026) 
+  bp_analysis(p, df_data, start_year = 1940, end_year = 2025) 
 }
 toc()
 stopCluster(cl)
